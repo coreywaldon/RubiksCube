1e45573a7f0d29cb78fbd2b6542b609f8fd50aa5
xtcNet <kappaxdxtc@gmail.com>
2015-12-10 10:50:42 -0600

    Update 12/10/15 10:49 AM.
    Added proper functionality for sliding rows and columns.
    
    TODO: Columns for faces 4 and 5

--- CubeLogic/Cube.java
+++ CubeLogic/Cube.java
@@ -29,7 +29,6 @@ public class Cube {
 
     }
     HashMap<Integer, Integer> cubeMap = new HashMap<Integer, Integer>();
-    HashMap<Integer, Integer> midCubeMap = new HashMap<Integer, Integer>();
     //boolean dimension refers to up or down; up: true, down: false
     public void slide(int side, int set, boolean isRow, boolean dimension) {
         switch (side) {
@@ -38,109 +37,215 @@ public class Cube {
             case 2:
             case 3: {
                 if (!isRow) {
-                    if (dimension) {
-                        for(int i=1;i<4;i++,side++) {
-                            int temp[] = {cubeArray[side][0][set], cubeArray[side][1][set], cubeArray[side][2][set]};
-                            if (side + 1 == 4)
-                                side = 0;
-                            cubeArray[side][0][set] = cubeArray[side+1][0][set];
-                            cubeArray[side][1][set] = cubeArray[side+1][1][set];
-                            cubeArray[side][2][set] = cubeArray[side+1][2][set];
-                            int k=0;
-                            for(int t:temp) {
-                                cubeArray[side - 1][k][set] = t;
-                                k++;
-                            }
-                        }
-                        if(set==0){
-                            int temp[]={cubeArray[4][0][0],cubeArray[4][0][1],cubeArray[4][0][2],
-                                    cubeArray[4][1][0],cubeArray[4][1][1],cubeArray[4][1][2],
-                                    cubeArray[4][2][0],cubeArray[4][2][1],cubeArray[4][2][2]};
-                            for(int i=0,c=0;i<3;i++)
-                                for(int j=0;j<3;j++,c++)
-                                    cubeArray[4][i][j]=temp[cubeMap.get(c)];
-                        }
-                        else if(set==1){
-                            int temp[]={cubeArray[0][0][1],cubeArray[0][1][1],cubeArray[0][2][1],
-                                        cubeArray[1][0][1],cubeArray[1][1][1],cubeArray[1][2][1],
-                                        cubeArray[2][0][1],cubeArray[2][1][1],cubeArray[2][2][1],
-                                        cubeArray[3][0][1],cubeArray[3][1][1],cubeArray[3][2][1]};
-                            for(int j = 0; j < 4; j++) {
-                                for (int i = 0; i < 3; i++) {
-                                    if(j==0)
-                                        cubeArray[0][i][1]=temp[i+9];
-                                    else
-                                        cubeArray[0][i][1]=temp[j+i-3];
-                                }
-                            }
-                        }
-                        else{
-                            int temp[]={cubeArray[5][0][0],cubeArray[5][0][1],cubeArray[5][0][2],
-                                    cubeArray[5][1][0],cubeArray[5][1][1],cubeArray[5][1][2],
-                                    cubeArray[5][2][0],cubeArray[5][2][1],cubeArray[5][2][2]};
-                            for(int i=0,c=0;i<3;i++)
-                                for(int j=0;j<3;j++,c++)
-                                    cubeArray[5][i][j]=temp[cubeMap.get(c)];
-                        }
-                    } else {
-                        for(int i=1;i<4;i++,side++) {
-                            int temp[] = {cubeArray[side][0][set], cubeArray[side][1][set], cubeArray[side][2][set]};
-                            if (side + 1 == 4)
-                                side = 0;
-                            cubeArray[side][0][set] = cubeArray[side-1][0][set];
-                            cubeArray[side][1][set] = cubeArray[side-1][1][set];
-                            cubeArray[side][2][set] = cubeArray[side-1][2][set];
-                            int k=0;
-                            for(int t:temp){
-                                cubeArray[side+1][k][set]=t;
-                                k++;
-                            }
-                        }
-                        if(set==0){
-                            int temp[]={cubeArray[4][0][0],cubeArray[4][0][1],cubeArray[4][0][2],
-                                    cubeArray[4][1][0],cubeArray[4][1][1],cubeArray[4][1][2],
-                                    cubeArray[4][2][0],cubeArray[4][2][1],cubeArray[4][2][2]};
-                            for(int i=0,c=0;i<3;i++)
-                                for(int j=0;j<3;j++,c++)
-                                    cubeArray[4][i][j]=temp[cubeMap.get(cubeMap.get(c))];
-                        }
-                        else if(set==1){
-                            int temp[]={cubeArray[0][0][1],cubeArray[0][1][1],cubeArray[0][2][1],
-                                    cubeArray[1][0][1],cubeArray[1][1][1],cubeArray[1][2][1],
-                                    cubeArray[2][0][1],cubeArray[2][1][1],cubeArray[2][2][1],
-                                    cubeArray[3][0][1],cubeArray[3][1][1],cubeArray[3][2][1]};
-                            for(int j = 0; j < 4; j++) {
-                                for (int i = 0; i < 3; i++) {
-                                    if(j==2)
-                                        cubeArray[0][i][1]=temp[j+i-9];
-                                    else
-                                        cubeArray[0][i][1]=temp[j+i+3];
-                                }
-                            }
-                        }
-                        else{
-                            int temp[]={cubeArray[5][0][0],cubeArray[5][0][1],cubeArray[5][0][2],
-                                    cubeArray[5][1][0],cubeArray[5][1][1],cubeArray[5][1][2],
-                                    cubeArray[5][2][0],cubeArray[5][2][1],cubeArray[5][2][2]};
-                            for(int i=0,c=0;i<3;i++)
-                                for(int j=0;j<3;j++,c++)
-                                    cubeArray[5][i][j]=temp[cubeMap.get(cubeMap.get(c))];
-                        }
-                    }
+                    slideColumn(set, side, dimension);
                 }
                 else{
+                    slideRow(set, side, dimension);
+                }
+            }
+            case 4:
+            case 5: {
+                if(!isRow){
                     if(dimension){
 
                     }
                     else{
-
+                        
+                    }
+                }
+                else{
+                    if(dimension){
+                        if(set==0)
+                            permuteFace(0, true);
+                        else if(set==2)
+                            permuteFace(2, true);
+                        int[] sides = {1,3,4,5};
+                        int currentSide=5;
+                        for(int i=0;i<sides.length;i++) {
+                            int temp[] = {cubeArray[i][set][0], cubeArray[i][set][1], cubeArray[i][set][2]};
+                            cubeArray[sides[i]][set][0] = cubeArray[currentSide][set][0];
+                            cubeArray[sides[i]][set][1] = cubeArray[currentSide][set][1];
+                            cubeArray[sides[i]][set][2] = cubeArray[currentSide][set][2];
+                            int k=0;
+                            for(int t:temp) {
+                                cubeArray[sides[i-1]][set][k] = t;
+                                k++;
+                            }
+                            currentSide=sides[i];
+                        }
+                    }
+                    else{
+                        if(set==0)
+                            permuteFace(0, false);
+                        else if(set==2)
+                            permuteFace(2, false);
+                        int[] sides = {5,4,3,1};
+                        int currentSide=1;
+                        for(int i=0;i<sides.length;i++) {
+                            int temp[] = {cubeArray[i][set][0], cubeArray[i][set][1], cubeArray[i][set][2]};
+                            cubeArray[sides[i]][set][0] = cubeArray[currentSide][set][0];
+                            cubeArray[sides[i]][set][1] = cubeArray[currentSide][set][1];
+                            cubeArray[sides[i]][set][2] = cubeArray[currentSide][set][2];
+                            int k=0;
+                            for(int t:temp) {
+                                cubeArray[sides[i+1]][set][k] = t;
+                                k++;
+                            }
+                            currentSide=sides[i];
+                        }
                     }
                 }
 
             }
-            case 4:
-            case 5: {
-
+        }
+    }
+    public void permuteFace(int side, boolean pos){
+        if(pos){
+            int temp[]={cubeArray[side][0][0],cubeArray[side][0][1],cubeArray[side][0][2],
+                    cubeArray[side][1][0],cubeArray[side][1][1],cubeArray[side][1][2],
+                    cubeArray[side][2][0],cubeArray[side][2][1],cubeArray[side][2][2]};
+            for(int i=0,c=0;i<3;i++)
+                for(int j=0;j<3;j++,c++)
+                    cubeArray[side][i][j]=temp[cubeMap.get(c)];
+        }else{
+            int temp[]={cubeArray[side][0][0],cubeArray[side][0][1],cubeArray[side][0][2],
+                    cubeArray[side][1][0],cubeArray[side][1][1],cubeArray[side][1][2],
+                    cubeArray[side][2][0],cubeArray[side][2][1],cubeArray[side][2][2]};
+            for(int i=0,c=0;i<3;i++)
+                for(int j=0;j<3;j++,c++)
+                    cubeArray[side][i][j]=temp[cubeMap.get(cubeMap.get(c))];
+        }
+    }
+    public void slideRow(int set, int side, boolean dimension){
+        if(dimension){
+            if(set==0){
+                if(side==0)
+                    permuteFace(3, true);
+                else
+                    permuteFace(side-1, true);
+            }
+            else if(set==2){
+                if(side==3)
+                    permuteFace(1,true);
+                else
+                    permuteFace(side+1,true);
+            }
+            for(int i=1;i<4;i++,side++) {
+                int temp[] = {cubeArray[side][set][0], cubeArray[side][set][1], cubeArray[side][set][2]};
+                int currentSide=side-1;
+                if (side==0)
+                    currentSide = 3;
+                cubeArray[side][set][0] = cubeArray[currentSide][set][0];
+                cubeArray[side][set][1] = cubeArray[currentSide][set][1];
+                cubeArray[side][set][2] = cubeArray[currentSide][set][2];
+                int k=0;
+                for(int t:temp) {
+                    cubeArray[side-1][set][k] = t;
+                    k++;
+                }
+            }
+        }
+        else{
+            if(set==0){
+                if(side==0)
+                    permuteFace(3, false);
+                else
+                    permuteFace(side-1, false);
+            }
+            else if(set==1){
+            }
+            else if(set==2) {
+                if (side == 3)
+                    permuteFace(1, false);
+                else
+                    permuteFace(side + 1, false);
+            }
+            for(int i=1;i<4;i++,side++) {
+                int temp[] = {cubeArray[side][set][0], cubeArray[side][set][1], cubeArray[side][set][2]};
+                int currentSide=side+1;
+                if (side==3)
+                    currentSide = 0;
+                cubeArray[side][set][0] = cubeArray[currentSide][set][0];
+                cubeArray[side][set][1] = cubeArray[currentSide][set][1];
+                cubeArray[side][set][2] = cubeArray[currentSide][set][2];
+                int k=0;
+                for(int t:temp) {
+                    cubeArray[side+1][set][k] = t;
+                    k++;
+                }
+            }
+        }
+    }
+    public void slideColumn(int set, int side, boolean dimension){
+        if (dimension) {
+            for(int i=1;i<4;i++,side++) {
+                int temp[] = {cubeArray[side][0][set], cubeArray[side][1][set], cubeArray[side][2][set]};
+                int currentSide=side-1;
+                if (side==3)
+                    currentSide = 0;
+                cubeArray[side][0][set] = cubeArray[currentSide][0][set];
+                cubeArray[side][1][set] = cubeArray[currentSide][1][set];
+                cubeArray[side][2][set] = cubeArray[currentSide][2][set];
+                int k=0;
+                for(int t:temp) {
+                    cubeArray[side - 1][k][set] = t;
+                    k++;
+                }
+            }
+            if(set==0){
+                permuteFace(4,true);
+            }
+            else if(set==1){
+                int temp[]={cubeArray[0][0][1],cubeArray[0][1][1],cubeArray[0][2][1],
+                        cubeArray[1][0][1],cubeArray[1][1][1],cubeArray[1][2][1],
+                        cubeArray[2][0][1],cubeArray[2][1][1],cubeArray[2][2][1],
+                        cubeArray[3][0][1],cubeArray[3][1][1],cubeArray[3][2][1]};
+                for(int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 3; i++) {
+                        if(j==0)
+                            cubeArray[0][i][1]=temp[i+9];
+                        else
+                            cubeArray[0][i][1]=temp[j+i-3];
+                    }
+                }
+            }
+            else{
+                permuteFace(5,false);
+            }
+        } else {
+            for(int i=1;i<4;i++,side++) {
+                int temp[] = {cubeArray[side][0][set], cubeArray[side][1][set], cubeArray[side][2][set]};
+                int currentSide=side-1;
+                if (side==0)
+                    currentSide = 3;
+                cubeArray[side][0][set] = cubeArray[currentSide][0][set];
+                cubeArray[side][1][set] = cubeArray[currentSide][1][set];
+                cubeArray[side][2][set] = cubeArray[currentSide][2][set];
+                int k=0;
+                for(int t:temp){
+                    cubeArray[side+1][k][set]=t;
+                    k++;
+                }
+            }
+            if(set==0){
+                permuteFace(4,true);
+            }
+            else if(set==1){
+                int temp[]={cubeArray[0][0][1],cubeArray[0][1][1],cubeArray[0][2][1],
+                        cubeArray[1][0][1],cubeArray[1][1][1],cubeArray[1][2][1],
+                        cubeArray[2][0][1],cubeArray[2][1][1],cubeArray[2][2][1],
+                        cubeArray[3][0][1],cubeArray[3][1][1],cubeArray[3][2][1]};
+                for(int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 3; i++) {
+                        if(j==2)
+                            cubeArray[0][i][1]=temp[j+i-9];
+                        else
+                            cubeArray[0][i][1]=temp[j+i+3];
+                    }
+                }
+            }
+            else{
+                permuteFace(5, false);
             }
         }
     }
